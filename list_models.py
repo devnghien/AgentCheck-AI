e05ea60499c5ce93fbbdfee324d04f500a3d85c0
Dev Nghien <devnghien@gmail.com>
2025-12-18 23:26:21 +0700

    Update

--- list_models.py
+++ list_models.py
@@ -1,7 +1,10 @@
-<<<<<<< HEAD
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "AIzaSyAchmL49SGwxgj0V6UeGzbPDjSVcRyTBiY"
+load_dotenv()
+# Lấy key từ file .env (file này sẽ nằm trong .gitignore)
+API_KEY=os.getenv("GGAI_API_KEY")
 url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
 
 response = requests.get(url)
@@ -14,21 +17,4 @@ if 'models' in models:
         if 'generateContent' in m['supportedGenerationMethods']:
             print(f"- {m['name']}")
 else:
-=======
-import requests
-
-API_KEY = "AIzaSyAchmL49SGwxgj0V6UeGzbPDjSVcRyTBiY"
-url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
-
-response = requests.get(url)
-models = response.json()
-
-if 'models' in models:
-    print("CÁC MODEL BẠN CÓ THỂ DÙNG:")
-    for m in models['models']:
-        # Chỉ lấy những model hỗ trợ tạo nội dung (generateContent)
-        if 'generateContent' in m['supportedGenerationMethods']:
-            print(f"- {m['name']}")
-else:
->>>>>>> dc83ea6796fa2aa4883397a1ddac607ce7af01dc
     print("Lỗi khi lấy danh sách:", models)
